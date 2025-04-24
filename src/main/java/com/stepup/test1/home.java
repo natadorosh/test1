4ca8cd7e0fcecee8ab06cc3a4df9019556dce3b6
natadorosh <git.user.kras_red@mail.ru>
2025-04-24 21:48:34 +0300

    commit1

---
+++ src/main/java/com/stepup/test1/home.java
@@ -0,0 +1,4 @@
+package com.stepup.test1;
+
+public class home {
+}
---
+++ src/main/java/com/stepup/test1/work.java
@@ -0,0 +1,15 @@
+package com.stepup.test1;
+
+public class work {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(11);
+        System.out.println(12);
+        System.out.println(13);
+    }
+}
